39a56fb42cb1b6eb8b68cf6dcbeda8797baeccd5
PetarPetrakiev93 <red_ferrari@mail.bg>
2016-12-16 21:52:42 +0200

    basic create content and add images

---
+++ Blog/Controllers/ImagesController.cs
@@ -0,0 +1,79 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class ImagesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [Route("Create")]
+        [HttpPost]
+        public ActionResult Create(Images model)
+        {
+            HttpPostedFileBase file = Request.Files["ImageData"];
+            Images service = new Images();
+            int i = service.UploadImageInDataBase(file, model);
+            if (i == 1)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    var authorId = database.Users.Where(u => u.UserName == this.User.Identity.Name).First().Id;
+                    model.AuthorId = authorId;
+                    database.Images.Add(model);
+                    database.SaveChanges();
+
+                    return RedirectToAction("List");
+                }
+                
+            }
+            return View(model);
+        }
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var content = database.Images.Include(a => a.Author).ToList();
+                return View(content);                
+            }
+                
+        }
+
+        private BlogDbContext db = new BlogDbContext();
+        public ActionResult RetrieveImage(int id)
+        {
+            byte[] cover = GetImageFromDataBase(id);
+            if (cover != null)
+            {
+                return File(cover, "image/jpg");
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public byte[] GetImageFromDataBase(int Id)
+        {
+            var q = from temp in db.Images where temp.Id == Id select temp.Image;
+            byte[] cover = q.First();
+            return cover;
+        }
+
+
+    }
+}
---
+++ Blog/Models/Images.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Models
+{
+    public class Images
+    {
+        [Key]
+        public int Id { get; set; }
+
+        
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [AllowHtml]
+        public string Contents { get; set; }
+
+        [Required]
+        public byte[] Image { get; set; }
+
+        [ForeignKey("Author")]
+        public string AuthorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+        private readonly BlogDbContext db = new BlogDbContext();
+        public int UploadImageInDataBase(HttpPostedFileBase file, Images contentViewModel)
+        {
+            contentViewModel.Image = ConvertToBytes(file);
+
+            var Content = new Images
+            {
+                Title = contentViewModel.Title,
+                Description = contentViewModel.Description,
+                Contents = contentViewModel.Contents,
+                Image = contentViewModel.Image,
+                
+            };
+            
+            
+            db.Images.Add(Content);
+
+            return 1;
+        }
+        public byte[] ConvertToBytes(HttpPostedFileBase image)
+        {
+            byte[] imageBytes = null;
+            BinaryReader reader = new BinaryReader(image.InputStream);
+            imageBytes = reader.ReadBytes((int)image.ContentLength);
+            return imageBytes;
+        }
+    }
+}
